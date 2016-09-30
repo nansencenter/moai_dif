ae0b8b9f4f8dfa848fde516e9604894a56fafdc4
Aleksander Vines <aleksander.vines@nersc.no>
2016-09-30 13:01:43 +0000

    Cleaned up. Using modified from svn.

--- moai_dif/xml.py
+++ moai_dif/xml.py
@@ -22,32 +22,4 @@ class XMLContent(object):
         document = svninfo.get() #urllib.urlopen(svninfo.path)
         root = etree.fromstring(document)
         xpath = XPath(root, nsmap={'x':'http://gcmd.gsfc.nasa.gov/Aboutus/xml/dif/'})
-        if not self.deleted:
-            self.modified = datetime.datetime.now()
-            #self.modified = parsed_time = xpath.date('//x:Last_DIF_Revision_Date')
-            #if parsed_time:
-            #    self.modified = util.parse_time(parsed_time[0].text)
-        #self.sets = self._get_sets(root)
         self.metadata = {'dif': etree.tostring(root)}
-        # # We validate that the  input file follows dif 9.8.4
-        # TODO: validate that it follows whatever schema/version its said to follow
-        #doc = etree.parse(path, parser=get_parser())
-        #xpath = XPath(doc, nsmap={'x':'http://gcmd.gsfc.nasa.gov/Aboutus/xml/dif/'})
-        #xpath = XPath(doc, nsmap={'x':'http://gcmd.gsfc.nasa.gov/Aboutus/xml/dif/'})
-        #self.root = doc.getroot()
-        #id = xpath.string('//x:Entry_ID')
-        #self.id = id
-        #self.modified = datetime.datetime.now()
-#         self.modified = xpath.date('//x:Last_DIF_Revision_Date')
-        #document_text = open(path).read()
-        #self.metadata = { 'dif' : document_text }
-
-
-def get_parser():
-    xsdPath = os.path.dirname(os.path.abspath(__file__)) + '/../dif_v9.8.4.xsd'
-    with open(xsdPath, "r") as myfile:
-        schemaString = myfile.read()
-    schema_root = etree.XML(schemaString)
-    schema = etree.XMLSchema(schema_root)
-    parser = etree.XMLParser(schema=schema)
-    return parser
